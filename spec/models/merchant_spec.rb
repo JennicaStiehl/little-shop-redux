0cd9901e120649c685475b0892865613cee7f2fa
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-13 16:09:15 -0600

    Added database cleaner

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,4 +1,12 @@
 RSpec.describe Merchant do
+  before :all do
+    DatabaseCleaner.clean
+  end
+
+  after :all do
+    DatabaseCleaner.clean
+  end
+
   describe 'Validate fields' do
     it 'should require a name' do
       merchant = Merchant.new
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,14 @@
+require 'bundler'
+
 ENV['RACK_ENV'] ||= 'test'
+Bundler.require(:default, :test)
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec'
 require 'capybara/dsl'
 
 Capybara.app = LittleShopApp
+DatabaseCleaner.strategy = :truncation
 
 RSpec.configure do |c|
   c.include Capybara::DSL
