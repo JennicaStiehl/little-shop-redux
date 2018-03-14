7c78c7c459fd3958c4d33103d752474a3ec12ffd
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-14 13:08:12 -0600

    Merchant seeding

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+require 'csv'
+require './app/models/merchant'
+
+CSV.foreach('./data/merchants.csv', headers: true, header_converters: :symbol) do |data|
+  hash = data.to_h
+  hash[:id] = hash[:id].to_i
+  Merchant.create(hash)
+end
