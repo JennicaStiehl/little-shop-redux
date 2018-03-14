bde429c8f0cfe465cca90968bb59fe16123f588c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-14 13:09:45 -0600

    Hound fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require 'csv'
 require './app/models/merchant'
 
-CSV.foreach('./data/merchants.csv', headers: true, header_converters: :symbol) do |data|
+CSV.foreach('./data/merchants.csv',
+            headers: true,
+            header_converters: :symbol) do |data|
   hash = data.to_h
   hash[:id] = hash[:id].to_i
   Merchant.create(hash)
