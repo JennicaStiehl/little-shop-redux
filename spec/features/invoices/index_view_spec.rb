ebbc401733c43b87ebbb5c721a9be6a2a12e6335
Evan Wheeler <anon0mys87@gmail.com>
2018-03-18 11:50:20 -0600

    First tests for invoice index view

--- spec/features/invoices/index_view_spec.rb
+++ spec/features/invoices/index_view_spec.rb
@@ -1,3 +1,27 @@
 RSpec.describe 'Invoice index' do
+  describe 'content' do
+    it 'should have a nav bar' do
+      visit '/invoices'
 
+      expect(page).to have_selector :css, 'nav p.title'
+    end
+
+    it 'should have a header partial' do
+      visit '/invoices'
+
+      expect(page).to have_selector :css, '.title-container'
+    end
+
+    it 'should have invoices' do
+      Invoice.create(merchant_id: 1, status: 'pending')
+      Invoice.create(merchant_id: 1, status: 'pending')
+
+      visit '/invoices'
+
+      within 'body' do
+        expect(page).to have_content '1'
+        expect(page).to have_content '2'
+      end
+    end
+  end
 end
