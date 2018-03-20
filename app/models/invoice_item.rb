1c3dd19d4a327237959a66c60f6f3f33f65b6523
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-20 13:31:28 -0600

    PRice formatting on invoice item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -6,4 +6,20 @@ class InvoiceItem < ActiveRecord::Base
                         :unit_price
   belongs_to :invoice
   belongs_to :item
+
+  def formatted_price
+    formatted = format('%2.2f', unit_price / 100.0)
+    top, bottom = formatted.split('.')
+
+    price_string = deliminate(top)
+
+    "$#{price_string}.#{bottom}"
+  end
+
+  def deliminate(top)
+    top.reverse!
+    split_string = top.scan(/.{1,3}/)
+    split_string.reverse!
+    split_string.map(&:reverse).join(',')
+  end
 end
