0ca2f4d1c1d32cb2315751c6fecd309f9a13a13d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-20 15:09:13 -0600

    Status percentages test

--- spec/features/invoices/dashboard_view_spec.rb
+++ spec/features/invoices/dashboard_view_spec.rb
@@ -41,9 +41,26 @@ RSpec.describe 'Invoice dashboard' do
     InvoiceItem.create(item_id: 2,invoice_id: 9,quantity: 1,unit_price: 2000)
 
     InvoiceItem.create(item_id: 1,invoice_id: 10,quantity: 1,unit_price: 1000)
+
+    visit '/invoices-dashboard'
   end
 
   after(:all) do
     DatabaseCleaner.clean
+  end
+
+  describe 'content' do
+    it 'should have cards' do
+      expect(page).to have_selector('.card')
+      expect(all('.card').length).to be(3)
+    end
+
+    it 'should have status percentages' do
+      within('.card:first-child') do
+        expect('.li:first-child').to have_content 'Pending 20%'
+        expect('.li:nth-child(2)').to have_content 'Shipped 70%'
+        expect('.li:last-child').to have_content 'Returned 10%'
+      end
+    end
   end
 end
