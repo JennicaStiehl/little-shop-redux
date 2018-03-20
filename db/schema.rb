8863d0be40a98033de292caec616db4e062a77a2
Evan Wheeler <anon0mys87@gmail.com>
2018-03-19 22:23:20 -0600

    Ran the migration to see what happens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319163639) do
+ActiveRecord::Schema.define(version: 20180320040515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180319163639) do
     t.datetime "updated_at", null: false
     t.integer "customer_id"
     t.integer "merchant_id"
+    t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
 
   create_table "items", force: :cascade do |t|
