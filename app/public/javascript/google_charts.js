e94918d1ea5a9caba9cb6567256a482d6c4d2496
Evan Wheeler <anon0mys87@gmail.com>
2018-03-20 20:23:58 -0600

    Added google chart to invoices dashboard

---
+++ app/public/javascript/google_charts.js
@@ -0,0 +1,30 @@
+// Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Status');
+        data.addColumn('number', 'Percent');
+        data.addRows([
+          ['Pending', 30],
+          ['Shipped', 57],
+          ['Returned', 13],
+        ]);
+
+        // Set chart options
+        var options = {'title':'Invoice Status Percentages',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
